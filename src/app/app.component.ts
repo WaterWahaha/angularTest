b50760e3ba2928015e06361affd567ac42e7ca9e
Edith <782558113@qq.com>
2019-07-25 11:15:49 +0800

    hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>
-                
+
             `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'WORLD'; }
